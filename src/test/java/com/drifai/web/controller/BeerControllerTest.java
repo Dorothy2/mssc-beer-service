cc32b9944b444235e3cfa987636c4332293f4326
Dorothy Rifai <drifai28@gmail.com>
2020-07-06 14:41:53 -0400

    Add mock BeerRepository to getBeerById() test

--- src/test/java/com/drifai/web/controller/BeerControllerTest.java
+++ src/test/java/com/drifai/web/controller/BeerControllerTest.java
@@ -1,31 +1,30 @@
 package com.drifai.web.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.UUID;
-
-import javax.validation.ConstraintViolationException;
-
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.drifai.domain.Beer;
+import com.drifai.repositories.BeerRepository;
+import com.drifai.web.model.BeerDto;
+import com.drifai.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.HttpStatus;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.drifai.web.model.BeerDto;
-import com.drifai.web.model.BeerStyleEnum;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigDecimal;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "com.drifai.web.mappers")
 class BeerControllerTest {
 
 	@Autowired
@@ -34,8 +33,13 @@ class BeerControllerTest {
 	@Autowired
 	ObjectMapper objectMapper;
 	
+	@MockBean
+	BeerRepository beerRepository;
+	
 	@Test
     void getBeerById() throws Exception {
+		given(beerRepository.findById(any())).willReturn
+		(Optional.of(Beer.builder().build()));
 		mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString())
 				.accept(MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk());
