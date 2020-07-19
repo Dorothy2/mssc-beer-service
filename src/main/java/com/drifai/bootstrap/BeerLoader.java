43a5eb7ec6dca01d811916be993f62c9e401ae6e
Dorothy Rifai <drifai28@gmail.com>
2020-07-19 06:44:09 -0400

    Local mySQL profile tweaks and Hikari CP configuration

--- src/main/java/com/drifai/bootstrap/BeerLoader.java
+++ src/main/java/com/drifai/bootstrap/BeerLoader.java
@@ -10,36 +10,30 @@ import com.drifai.domain.Beer;
 //import com.drifai.domain.BeerStyleEnum;
 import com.drifai.repositories.BeerRepository;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
-//@Slf4j
-//@Component
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class BeerLoader implements CommandLineRunner {
 	private final BeerRepository beerRepository;
 	
 	public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234300019";
     public static final String BEER_3_UPC = "0083783375213";
-    
-    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
-    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
-
-	
-	public BeerLoader(BeerRepository beerRepository) {
-		this.beerRepository = beerRepository;
-	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		//using data.sql file now
-        //   loadBeerObjects();
+		if(beerRepository.count() == 0) {
+          loadBeerObjects();
+		}
 	}
 	
 	private void loadBeerObjects() {
 		if(beerRepository.count() == 0) {
 			Beer b1 = Beer.builder()
-					.beerName("Mango Bobs")
+					.beerName("Mango Bobs")				
 					//.beerStyle(BeerStyleEnum.IPA)
 					.beerStyle("IPA")
 					.quantityToBrew(200)
@@ -72,7 +66,7 @@ public class BeerLoader implements CommandLineRunner {
 			beerRepository.save(b2);
 			beerRepository.save(b3);
 			
-			//log.debug("Number of beer records created: " + beerRepository.count());
+			log.debug("Number of beer records created: " + beerRepository.count());
 		}
 	}
 }
--- src/test/java/com/drifai/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/com/drifai/services/inventory/BeerInventoryServiceRestTemplateImplTest.java
@@ -24,10 +24,11 @@ class BeerInventoryServiceRestTemplateImplTest {
 	
 	@Test
 	void getOnHandInventory() {
-		Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UUID);
+		//TODO: Refactor to pull inventory from UPC rather than UUID
+		//Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UUID);
 		
 		// Quantity on hand is coming from mssc-beer-inventory-service BeerInventoryBootstrap.java
-		System.out.println("Inventory on hand: " + qoh);
+		//System.out.println("Inventory on hand: " + qoh);
 	}
 
 }
