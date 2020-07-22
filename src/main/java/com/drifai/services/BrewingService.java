b55af2503eed3250aecb9270d1251e503e84c81e
Dorothy Rifai <drifai28@gmail.com>
2020-07-22 10:40:25 -0400

    Brewing service, task to check inventory qoh, and ArtemisAutoConfiguration.class fix

---
+++ src/main/java/com/drifai/services/BrewingService.java
@@ -0,0 +1,46 @@
+package com.drifai.services;
+
+import java.util.List;
+
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+import com.drifai.config.JmsConfig;
+import com.drifai.domain.Beer;
+import com.drifai.events.BrewBeerEvent;
+import com.drifai.mappers.BeerMapper;
+import com.drifai.repositories.BeerRepository;
+import com.drifai.services.inventory.BeerInventoryService;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class BrewingService {
+	
+	private final BeerRepository beerRepository;
+	private final BeerInventoryService beerInventoryService;
+	private final JmsTemplate jmsTemplate;
+	private final BeerMapper beerMapper;
+	
+	@Scheduled(fixedRate = 5000) // every 5 seconds
+	public void checkForLowInventory() {
+		List<Beer> beers = beerRepository.findAll();
+		
+		beers.forEach(beer -> {
+			Integer invQOH = beerInventoryService.getOnHandInventory(beer.getId());
+			
+			log.debug("Min Onhand is: " + beer.getMinOnHand());
+			log.debug("Inventory is: " + invQOH);
+			
+			if(beer.getMinOnHand() >= invQOH) {
+				jmsTemplate.convertAndSend(JmsConfig.BREWING_REQUEST_QUEUE, 
+					new BrewBeerEvent(beerMapper.beerToBeerDto(beer)));
+			}
+		});
+	}
+
+}
