f7d07b68fdb7af3fb104351e968267dd8c9653bb
Dorothy Rifai <drifai28@gmail.com>
2020-12-28 16:08:22 -0500

    Update inventory service to use OpenFeign

---
+++ src/main/java/com/drifai/services/inventory/InventoryServiceFeignClient.java
@@ -0,0 +1,20 @@
+package com.drifai.services.inventory;
+
+import java.util.List;
+import java.util.UUID;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.drifai.services.inventory.model.BeerInventoryDto;
+
+@FeignClient(name = "inventory-service")
+public interface InventoryServiceFeignClient {
+	
+	@RequestMapping(method = RequestMethod.GET, value = BeerInventoryServiceRestTemplateImpl.INVENTORY_PATH)
+	ResponseEntity<List<BeerInventoryDto>> getOnHandInventory(@PathVariable UUID beerId);
+
+}
